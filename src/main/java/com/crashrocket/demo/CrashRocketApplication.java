5568e26a1963cbf99aebf71a97e44d9b32e93cf6
NickVik04 <nikhilshrm49@gmail.com>
2025-10-05 17:03:46 +0530

    changed the url of UI game

--- src/main/java/com/crashrocket/demo/CrashRocketApplication.java
+++ src/main/java/com/crashrocket/demo/CrashRocketApplication.java
@@ -19,7 +19,7 @@ public class CrashRocketApplication {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedOrigins("http://localhost:3000");
+                registry.addMapping("/**").allowedOrigins("https://rocket-frontend-seven.vercel.app");
             }
         };
     }
